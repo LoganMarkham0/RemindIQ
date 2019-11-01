9872be0fd154bd4da30e8a9ab9bb70c1dc6a0fc8
Wyatt Powell <POWELLW1@ETSU.EDU>
2019-11-01 15:11:06 -0400

    Fixed Database and added some Location and Database functionalities.

--- RemindIQ/RemindIQ/Models/Reminder.cs
+++ RemindIQ/RemindIQ/Models/Reminder.cs
@@ -3,25 +3,24 @@
 namespace RemindIQ.Models
 {
     public class Reminder 
-        {
-            [PrimaryKey, AutoIncrement]
-            public int Id { get; set; }
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
 
-            public string Name { get; set; }
+        public string Name { get; set; }
 
-            public string DestinationAddress { get; set; }
+        public string DestinationAddress { get; set; }
 
-            public double Latitude { get; set; }
+        public double Latitude { get; set; }
             
-            public double Longitude { get; set; }
+        public double Longitude { get; set; }
 
-            public double Range { get; set; }
+        public double Range { get; set; }
 
-            public double DistanceToDestination { get; set; }
+        public double DistanceToDestination { get; set; }
 
-            public string Notes { get; set; }
+        public string Notes { get; set; }
 
-            public int Status { get; set; }
-        }
-    
+        public int Status { get; set; }
+    }
 }
